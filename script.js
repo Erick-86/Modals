2839ffe0cef715964a000a4149d8ec5f40edaa71
FreeMan <zelonaire@gmail.com>
2023-02-20 08:54:30 +0000

    storing elements into variables

---
+++ script.js
@@ -0,0 +1,4 @@
+const showModal = document.querySelectorAll(".show-modal");
+const overLay = document.getElementById("overlay");
+const modal = document.getElementById("modal-container");
+const closeModal = document.getElementById("close-btn");
