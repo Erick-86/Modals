2f418bd1be884905e5edb1ababb9da7f079dd2ab
FreeMan <zelonaire@gmail.com>
2023-02-20 09:01:54 +0000

    showing and hiding modal functions

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ const showModalBnts = document.querySelectorAll(".show-modal");
 const overLay = document.getElementById("overlay");
 const modal = document.getElementById("modal-container");
 const closeModal = document.getElementById("close-btn");
+
+//Showing and Hiding Modal function
+//Showing
+const showModal = function () {
+  modal.style.display = 'block'
+  overLay.style.display = 'block'
+}
+
+//Hiding
+const hideModal = function () {
+  modal.style.display = 'none'
+  overLay.style.display = 'none'
+}
